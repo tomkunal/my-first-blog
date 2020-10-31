5b46ee6b3eefd21deca4ad4019f186525d524da9
tomkunal <73659893+tomkunal@users.noreply.github.com>
2020-10-31 13:34:01 +0530

    Update settings.py

--- devtestDBcheck/settings.py
+++ devtestDBcheck/settings.py
@@ -84,6 +84,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'devtestDBcheck.urls'
 
+SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
+# Find templates in the same folder as settings.py.
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
