4d28841a6cb1d7e9b1dc5aa51740775ee2ab02be
tomkunal <73659893+tomkunal@users.noreply.github.com>
2020-10-31 14:08:28 +0530

    Update settings.py

--- devtestDBcheck/settings.py
+++ devtestDBcheck/settings.py
@@ -69,7 +69,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django_filters',
+   # 'django.contrib.staticfiles',
     
 ]
 
